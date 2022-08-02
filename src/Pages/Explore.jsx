73de29572558fd86ca2c6b1c2f147bb6b4b5405c
Raymond-techy <adunoloyerahmantaiwo200208@gmail.com>
2022-08-02 21:40:46 +0100

    changes added

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -88,7 +88,19 @@ function Explore() {
           </svg>
           Tecno
         </Link>
-        <Link to="/category/itel">itel</Link>
+        <Link to="/category/itel">
+          {" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 384 512"
+            width="30px"
+            height="32px"
+            className="mx-auto"
+          >
+            <path d="M384 64.01c0 17.67-14.33 32-32 32h-128v352c0 17.67-14.33 31.99-32 31.99s-32-14.32-32-31.99v-352H32c-17.67 0-32-14.33-32-32s14.33-32 32-32h320C369.7 32.01 384 46.34 384 64.01z" />
+          </svg>
+          itel
+        </Link>
       </div>
       <div>
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
